c894f70fa1d9cc0c14731037637a80be609e4c8e
amolnagose12 <amolnagose12@gmail.com>
2021-12-07 18:07:35 +0530

    Set event listners when document is loaded

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -15,7 +15,7 @@ class EmployeePayrollData {
         if (nameRegex.test(name))
             this._name = name;
         else
-            throw new Error("Name is Incorrect");
+            throw new Error("Name is incorrect  Rule : 1st letter should in caps,  Should contain min 3 letter");
     }
 
     get profilePic() {
@@ -48,9 +48,16 @@ class EmployeePayrollData {
     }
 
     set salary(salary) {
-
         this._salary = salary;
+    }
 
+    getTodayDate=()=>{
+        var objToday = new Date(),
+        dayOfMonth = objToday.getDate(),
+        months = new Array('jan', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'aug', 'sep', 'oct', 'nov', 'dec'),
+        curMonth = months[objToday.getMonth()],
+        curYear = objToday.getFullYear();
+        return dayOfMonth + "-" + curMonth + "-" + curYear;
     }
 
     get startDate() {
@@ -58,7 +65,17 @@ class EmployeePayrollData {
     }
 
     set startDate(startDate){
-        this._startDate=startDate;
+        let currentDate = new Date();
+        if (startDate > currentDate) {
+            throw new Error("Start date is a future date");
+        } else {
+            let differnce = Math.abs(currentDate.getTime() - startDate.getTime());
+            let date = differnce / (1000 * 60 * 60 * 24);
+            if (date > 30) {
+                throw new Error("Start date is beyond 30 days");
+            }
+        }
+        this._startDate = startDate;
     }
     
     get notes() {
@@ -72,8 +89,8 @@ class EmployeePayrollData {
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const employeeDate = this.startDate == undefined ? "undefined" : 
-        this.startDate.toLocaleDateString("en-us", options);
-        return "id=" + this.id + "Name = " + this.name + ", profilePic = " + this.profilePic + ", Gender = " + this.gender + 
-        ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
+                                this.startDate.toLocaleDateString("en-us", options);
+        return "id=" + this.id + "name = " + this.name + ", profilePic = " + this.profilePic + ", gender = " + this.gender + 
+        ", department = " + this.department + ", salary = " + this.salary + ", Start Date = " + employeeDate + ", notes = " + this.notes;
     }
 }
