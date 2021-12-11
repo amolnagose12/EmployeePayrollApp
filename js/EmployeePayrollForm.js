cd85ae70d2f2cbd0e421a86a882475abb74d1569
amolnagose12 <amolnagose12@gmail.com>
2021-12-11 16:45:12 +0530

    details update

--- js/EmployeePayrollForm.js
+++ js/EmployeePayrollForm.js
@@ -1,3 +1,5 @@
+let isUpdate = false;
+let employeePayrollObj = {};
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector('#name');
     name.addEventListener('input', function() {
@@ -14,10 +16,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
 
     const salary = document.querySelector('#salary');
-    const output = document.querySelector('.salary-output');
+    setTextValue('.salary-output',salary.value);
     output.textContent = salary.value;
     salary.addEventListener('input', function() {
-        output.textContent = salary.value;
+        setTextValue('.salary-output',salary.value);
     });
 
     const date = document.querySelector('#date');
@@ -33,7 +35,44 @@ window.addEventListener('DOMContentLoaded', (event) => {
             setTextValue('.dates-error',e);
         }
     });
+    checkforUpdate();
 });
+
+const setSelectedValues=(propertyValue,value)=>{
+    let allItems=document.querySelectorAll(propertyValue);
+    allItems.forEach(item =>{
+        if(Array.isArray(value)){
+            if(value.includes(item.value)){
+                item.checked=true;
+            }
+        }
+        else if(item.value === value)
+            item.checked=true;
+    });
+}
+
+const checkforUpdate = () => {
+    const employeePayrollJson = localStorage.getItem('editEmp');
+    isUpdate = employeePayrollJson ? true : flase;
+    if (!isUpdate)
+        return;
+    employeePayrollObj = JSON.parse(employeePayrollJson);
+    setForm();
+}
+
+const setForm = () => {
+    setValue('#name', employeePayrollObj._name);
+    setSelectedValues('[name=profile]',employeePayrollObj._profilePic);
+    setSelectedValues('[name=gender]',employeePayrollObj._gender);
+    setSelectedValues('[name=department]',employeePayrollObj._department);
+    setValue('#salary', employeePayrollObj._salary);
+    setTextValue('.salary-output', employeePayrollObj._salary)
+    setValue('#notes', employeePayrollObj._notes);
+    let date = stringifyDate(employeePayrollObj._startDate).split(" ");
+    setValue('#day', date[0]);
+    setValue('#month', date[1]);
+    setValue('#year', date[2]);
+}
 
 function save(event) {
     alert("save")
